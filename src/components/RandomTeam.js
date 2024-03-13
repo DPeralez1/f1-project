29025de2e9078dc89918a95260fd237b5c83a775
David Peralez <davidperalez89@gmail.com>
2024-03-13 12:02:48 -0700

    added fluid images

--- src/components/RandomTeam.js
+++ src/components/RandomTeam.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import {TEAM_LIST} from '../TEAM_LIST'
+import Image from 'react-bootstrap/Image';
 
 const RandomTeamPicker = () => {
-    const pickRandomTeam = () => {
-      const randomIndex = Math.floor(Math.random() * TEAM_LIST.length);
-      return TEAM_LIST[randomIndex].team_name;
-    };
+  const randomIndex = Math.floor(Math.random() * TEAM_LIST.length);
+  const randomTeam = TEAM_LIST[randomIndex];
+    
   
     return (
       <div className='m-4'>
         <h2>Your Team for this weekend!</h2>
-        <p className="display-6 text-danger fw-bold">{pickRandomTeam()}</p>
+        <p className="display-6 text-danger fw-bold">{randomTeam.team_name}</p>
+        <Image src={randomTeam.image} fluid />
       </div>
     );
   };
